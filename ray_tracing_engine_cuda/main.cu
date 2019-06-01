87c5b5f46166b3f0773c31f54e3b4dad0fbf605e
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-06-01 11:19:32 +0800

    add ray.cuh

--- ray_tracing_engine_cuda/main.cu
+++ ray_tracing_engine_cuda/main.cu
@@ -1,5 +1,6 @@
-#include "PNGMaster.h"
-#include "Vector3.h"
+#include "pngmaster.h"
+#include "ray.cuh"
+#include "vector3.cuh"
 #include <iostream>
 #include <time.h>
 // limited version of checkCudaErrors from helper_cuda.h in CUDA examples
@@ -16,15 +17,24 @@ void check_cuda(cudaError_t result, char const *const func,
   }
 }
 
-__global__ void render(float *fb, int max_x, int max_y) {
+__device__ vector3 color(const ray &r) {
+  vector3 unit_direction = r.direction().normalize();
+  float t = 0.5f * (unit_direction.y() + 1.0f);
+  return (1.0f - t) * vector3(1, 1, 1) + t * vector3(0.0, 0.0, 0.0);
+}
+
+__global__ void render(vector3 *fb, int max_x, int max_y,
+                       vector3 lower_left_corner, vector3 horizontal,
+                       vector3 vertical, vector3 origin) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
   if ((i >= max_x) || (j >= max_y))
     return;
-  int pixel_index = j * max_x * 3 + i * 3;
-  fb[pixel_index + 0] = float(i) / max_x;
-  fb[pixel_index + 1] = float(j) / max_y;
-  fb[pixel_index + 2] = 0.2;
+  int pixel_index = j * max_x + i;
+  float u = float(i) / float(max_x);
+  float v = float(j) / float(max_y);
+  ray tmp_r(origin, lower_left_corner + u * horizontal + v * vertical);
+  fb[pixel_index] = color(tmp_r);
 }
 
 int main() {
@@ -37,10 +47,10 @@ int main() {
   std::cerr << "in " << tx << "x" << ty << " blocks.\n";
 
   int num_pixels = nx * ny;
-  size_t fb_size = 3 * num_pixels * sizeof(float);
+  size_t fb_size = num_pixels * sizeof(vector3);
 
   // allocate FB
-  float *fb;
+  vector3 *fb;
   checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
   clock_t start, stop;
@@ -48,7 +58,9 @@ int main() {
   // Render our buffer
   dim3 blocks(nx / tx + 1, ny / ty + 1);
   dim3 threads(tx, ty);
-  render<<<blocks, threads>>>(fb, nx, ny);
+  render<<<blocks, threads>>>(fb, nx, ny, vector3(-2.0f, -1.0f, -1.0f),
+                              vector3(4.0f, 0.0f, 0.0f), vector3(0.0, 2.0, 0.0),
+                              vector3(0.0f, 0.0f, 0.0f));
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
   stop = clock();
@@ -56,19 +68,17 @@ int main() {
   std::cerr << "took " << timer_seconds << " seconds.\n";
 
   // Output FB as Image
-  PNGMaster myImage(ny, nx);
+  pngmaster myImage(ny, nx);
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      size_t pixel_index = j * 3 * nx + i * 3;
-      Vector3 tmp_vec(fb[pixel_index + 0], fb[pixel_index + 1],
-                      fb[pixel_index + 2]);
-      tmp_vec *= 255.99;
-      myImage.setPixel(i, j, tmp_vec[0], tmp_vec[1], tmp_vec[2]);
+      size_t pixel_index = j * nx + i;
+      vector3 tmp_vec = fb[pixel_index] * 255.99f;
+      myImage.set_pixel(i, j, tmp_vec.r(), tmp_vec.g(), tmp_vec.b());
     }
   }
 
   string file_name = "test" + std::to_string(timer_seconds) + ".png";
-  myImage.genPNG(file_name.c_str());
+  myImage.output(file_name.c_str());
   std::cerr << "render finished" << std::endl;
 
   checkCudaErrors(cudaFree(fb));
